9e07fd4b601e4810fb4bff67507955f45c4fa324
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-03 00:03:32 -0600

    testing pycodestyle, mypy, with given main , and documentation - all 200

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""
+5. Complex types - list of floats
+
+Write a type-annotated function sum_list
+which takes a list input_list of floats as argument
+and returns their sum as a float.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    :Function sum_list: sums a list of floats
+
+    :param input_list: list of floats
+    :Return: sum of list of floats
+    """
+    return sum(input_list)
