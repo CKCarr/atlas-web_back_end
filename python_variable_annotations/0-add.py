45da3da151e2e98377a3fa5534377fbca8c8ced0
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-02 23:08:49 -0600

    additions to python_variable_annotations Tested mypy, pycodestyle, test of main and documentation all 200

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""
+This is a module for performing basic addition arithmetic operations.
+
+Task 0. Basic annotations - add
+
+Write a type-annotated function add that takes a float a and a float b as
+arguments and returns their sum as a float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two floating-point numbers and return the result.
+
+    :param a: First floating-point number
+    :param b: Second floating-point number
+    :return: Sum of a and b
+    """
+    return a + b
