b011d5f4f70f7f9c12a8a2699050ef28de702c66
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-02 23:18:36 -0600

    Tested mypy, pycodestyle, test of main and documentation all 200

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+
+"""
+Task 1. Basic annotations - concat
+
+Write a type-annotated function concat
+that takes a string str1 and a string str2 as arguments
+and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ concat function
+    concatenates two strings and returns the result
+
+    Args:
+        str1 (str): String 1
+        str2 (str): String 2
+
+    Returns:
+        str: result String from concatenation
+    """
+    return str1 + str2
