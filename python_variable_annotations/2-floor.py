5b5a78f0097b7e091fc82fff97c68596b7afef54
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-02 23:34:15 -0600

    Tested mypy, pycodestyle, test of main and documentation all 200

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+import math
+
+"""
+2. Basic annotations - floor
+
+Write a type-annotated function floor
+which takes a float n as argument
+and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """ float function which returns the floor of the float
+
+    Args:
+        n (float): the given float to be floored
+
+    Returns:
+        int: result of the floor operation
+    """
+    return math.floor(n)
--- python_variable_annotations/2-main.py
+++ python_variable_annotations/2-main.py
@@ -6,6 +6,7 @@ floor = __import__('2-floor').floor
 
 ans = floor(3.14)
 
+print(ans)
 print(ans == math.floor(3.14))
 print(floor.__annotations__)
 print("floor(3.14) returns {}, which is a {}".format(ans, type(ans)))
