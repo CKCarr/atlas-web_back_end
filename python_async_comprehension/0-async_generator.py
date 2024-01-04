11662997ff0bc4545edb8d7ff2e816756a7f32db
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-04 13:28:36 -0600

    testing

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -22,8 +22,8 @@ async def async_generator():
     :param None: None
     :return: 10 random numbers between 0 and 10 - may be float
     """
-    for _ in range(10):
+    for i in range(10):
         # wait asynchronously for 1 second
         await asyncio.sleep(1)
         # yield a random number between 0 and 10
-        yield random.randint(0, 10)
+        yield i * random.random()
