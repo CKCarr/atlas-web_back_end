20a47cc0cff5ce4383648c0e22aafbf8e4b732aa
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-03 23:52:46 -0600

    testing mearure_runtime , main wasn't eworking to test against had to revise 🤞🤞

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,1 +1,36 @@
 #!/usr/bin/env python3
+"""
+From the previous file, import wait_n into 2-measure_runtime.py.
+
+Create a measure_time function
+with integers n and max_delay as arguments
+that measures the total execution time for wait_n(n, max_delay),
+and
+returns total_time / n.
+Your function should return a float.
+
+Use the time module to measure an approximate elapsed time.
+"""
+
+import asyncio
+import time
+
+wait_n = __import__("1-concurrent_coroutines").wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    Function measure_time that measures the total execution time
+    for wait_n(n, max_delay) and returns total_time / n
+    Args:
+        n (int): number of times to call wait_random
+        max_delay (int): max wait time for each call to wait_random
+
+    Returns:
+        float: total_time / n
+    """
+    start_time = time.perf_counter()
+    await wait_n(n, max_delay)
+    end_time = time.perf_counter()
+    total_time = end_time - start_time
+    return total_time / n
