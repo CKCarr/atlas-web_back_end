d21f66c3059d6f80c698bcebc54f8bdfd5904979
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-04 13:43:01 -0600

    I think Ive done it watson

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -11,9 +11,10 @@ Use the random module.
 """
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
-async def async_generator() -> Generator[float, None, None]:
+
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     Function async_generator coroutine that will loop 10 times,
     each time asynchronously wait 1 second,
