abed240b5271ac906d8e8b3ae81bad8ae66b0fc3
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-09 11:15:00 -0600

    implement a child class BasicCache to build off parent BaseCaching

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -15,7 +15,8 @@ def get(self, key):
 Must return the value in self.cache_data linked to key.
 If key is None or if the key doesn’t exist in self.cache_data, return None.
 """
-from  import BaseCaching
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
@@ -28,12 +29,29 @@ class BasicCache(BaseCaching):
         put: method that adds an item to the cache. If the key or the item is non the method does nothing.
         get: method that gets an item from the cache by it's key. If the key is 'None' or doesn't exist in the cache, it returns 'None'.
     """
-    
+
     def put(self, key, item):
         """ put method - adds an item to the cache.
         must assign to the dictionary self.cache_data
 
         Args:
-            key (): _description_
-            item (_type_): _description_
+            key (str): key to add to the cache
+            item (any): item to add to the cache
         """
+        # checks - if key or item is not none
+        if key is not None and item is not None:
+            # assigns item to key in cache of self.cache_data dictionary
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ get method - gets an item from the cache by it's key.
+
+        Args:
+            key (str): key to get from the cache
+        """
+        # checks if key is not None and if key exists in the cache
+        if key is not None and key in self.cache_data:
+            # return the value of the key from cache
+            return self.cache_data[key]
+        # if the key doesn't exist in cache or is None, return None
+        return None
