7b8690e9280f6bc2a66e411ae9bbf99d42abaf9f
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-13 01:57:09 -0600

    commit message: initialize repo and test the filtered_logger

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import re
+"""
+Write a function called filter_datum
+that returns the log message obfuscated:
+
+Arguments:
+fields:
+    a list of strings representing all fields to obfuscate
+    
+redaction:
+    a string representing by what the field will be obfuscated
+    
+message:
+    a string representing the log line
+    
+separator:
+    a string representing by which character is separating
+    all fields in the log line (message)
+    
+The function should use a regex
+    to replace occurrences of certain field values.
+    
+filter_datum should be less than 5 lines long
+    and use re.sub to perform the substitution
+    with a single regex.
+"""
+
+
+def filter_datum(fields: str, redaction : str, message : str, separator: str ) -> str:
+    """ returns the lob message obfuscated """
+    for field in fields:
+        message = re.sub(f"{field}=[^{separator}]+", f"{field}={redaction}", message)
+    return message
