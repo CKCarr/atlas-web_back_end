90d39c797f85d6684a24ee7ea53dfb9b73c81de3
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-13 21:53:53 -0600

    6. Check valid password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -56,6 +56,7 @@ def hash_password(password: str) -> bytes:
     # return the hashed password
     return hashed_password
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ function is_valid
     Validates that the provided password matches the hashed password.
@@ -69,7 +70,7 @@ def is_valid(hashed_password: bytes, password: str) -> bool:
     """
     # convert the password string to bytes
     password_bytes = password.encode('utf-8')
-    
+
     #  check if provided password
     # matches the hashed password
     return bcrypt.checkpw(password_bytes, hashed_password)
