909d0142c3cf839c325555d9cd00ad4f0dce2b1a
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-22 02:39:42 -0600

    2. Create a session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -7,7 +7,7 @@
         SessionAuth: The SessionAuth class which inherits from Auth
 """
 from .auth import Auth
-
+import uuid
 
 class SessionAuth(Auth):
     """ SessionAuth class
@@ -15,4 +15,25 @@ class SessionAuth(Auth):
     Args:
         Auth (class) : Inherits from Auth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a Session ID for a user_id
+
+        Args:
+            user_id (str, optional): User ID. Defaults to None.
+
+        Returns:
+            str: Session ID
+        """
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        # Generate a session ID
+        session_id = str(uuid.uuid4())
+        # Link the user_id to the session_id
+        self.user_id_by_session_id[session_id] = user_id
+
+        # Return the session ID
+        return session_id
