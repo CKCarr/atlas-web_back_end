ebfe750b1bc578c69bb051d5539aef8f22ecd52a
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-22 21:00:19 -0600

    initiallize User authentication service

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,7 +2,8 @@
 """ 6. Basic Flask app
 In this task, you will set up a basic Flask app.
 
-Create a Flask app that has a single GET route ("/") and use flask.jsonify to return a JSON payload of the form:
+Create a Flask app that has a single GET route ("/") and use flask.
+jsonify to return a JSON payload of the form:
 """
 from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
@@ -17,6 +18,7 @@ app = Flask(__name__)
 def hello_world():
     """ Hello world """
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
