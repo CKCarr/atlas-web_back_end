69faae9856fb12cdd900669e31b740bd0b7d3400
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-23 15:15:41 -0600

    7. Register user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -20,5 +20,17 @@ def hello_world():
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """ Register user """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        new_user = AUTH.register_user(email, password)
+        return jsonify({"email": new_user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
