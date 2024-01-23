da898983d9762f95ad659422f0a32f23ee16b802
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-22 22:55:31 -0600

    propagate = False

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -6,13 +6,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from user import Base, User
 
 # Set SQLAlchemy log level: only show error messages and above
-logging.getLogger('sqlalchemy').setLevel(logging.ERROR)
-
-
-from user import Base, User
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').propagate = False
 
 
 class DB:
@@ -22,7 +20,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -44,9 +42,10 @@ class DB:
         # Create a new user object
         new_user = User(email=email,
                         hashed_password=hashed_password)
+        session = self._session
         # Add the user to the session object
-        self._session.add(new_user)
+        session.add(new_user)
         # Save the user to the database
-        self._session.commit()
+        session.commit()
         #  Return newly created user object
         return new_user
