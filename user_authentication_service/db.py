72f2b89e6c435ff167033b18e97315b2bf154fdc
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-22 21:52:29 -0600

    1. create user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-from user import Base
+from user import Base, User
 
 
 class DB:
@@ -29,3 +29,18 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """ Method that saves an user to the database
+        adding new user to the session,
+        saves it to the database
+        """
+        # Create a new user object
+        new_user = User(email=email,
+                        hashed_password=hashed_password)
+        # Add the user to the session object
+        self._session.add(new_user)
+        # Save the user to the database
+        self._session.commit()
+        #  Return newly created user object
+        return new_user
