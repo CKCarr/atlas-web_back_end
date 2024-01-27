78d323570120bb86bd30f5ca750a30f08784cb22
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-27 00:00:42 -0600

    0. Parameterize a unit test

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -6,6 +6,7 @@ import unittest
 from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
 
+
 class TestAccessNestedMap(unittest.TestCase):
     """ TestAccessNestedMap class
 
@@ -13,11 +14,10 @@ class TestAccessNestedMap(unittest.TestCase):
         unittest (unittest.TestCase): Unit testing for utils.py
     """
     @parameterized.expand([
-        ({'a': 1}, {'a',}, 1),
-        ({'a': {'b': 2}}, {'a',}, {'b': 2}),
-        ({'a': {'b': 2}}, {'a', 'b'}, 2),
+        ({'a': 1}, ('a',), 1),
+        ({'a': {'b': 2}}, ('a',), {'b': 2}),
+        ({'a': {'b': 2}}, ('a', 'b'), 2),
     ])
-
     def test_access_nested_map(self, nested_map, path, expected):
         """ Test access_nested_map method for utils.py
         check if the method returns the expected result
@@ -26,6 +26,7 @@ class TestAccessNestedMap(unittest.TestCase):
         result = access_nested_map(nested_map, path)
         # assert that it returns as expected
         self.assertEqual(result, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
