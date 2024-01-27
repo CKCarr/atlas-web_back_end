8d2f13c282fd833da874e05606fd3fbe20bad6f6
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-26 23:45:42 -0600

    0. Parameterize a unit test

---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Generic utilities for github org client.
+"""
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map, get_json, memoize
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ TestAccessNestedMap class
+
+    Args:
+        unittest (unittest.TestCase): Unit testing for utils.py
+    """
+    @parameterized.expand([
+        ({'a': 1}, {'a',}, 1),
+        ({'a': {'b': 2}}, {'a',}, {'b': 2}),
+        ({'a': {'b': 2}}, {'a', 'b'}, 2),
+    ])
+
+    def test_access_nested_map(self, nested_map, path, expected):
+        """ Test access_nested_map method for utils.py
+        check if the method returns the expected result
+        """
+        # call the function with nested_map and path
+        result = access_nested_map(nested_map, path)
+        # assert that it returns as expected
+        self.assertEqual(result, expected)
+
+if __name__ == '__main__':
+    unittest.main()
--- Unittests_and_integration_tests/utils.py
+++ Unittests_and_integration_tests/utils.py
@@ -10,6 +10,9 @@ from typing import (
     Dict,
     Callable,
 )
+from collections import namedtuple
+from collections.abc import Mapping, MutableMapping
+
 
 __all__ = [
     "access_nested_map",
