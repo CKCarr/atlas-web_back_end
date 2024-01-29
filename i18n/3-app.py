36131a4dc786e83b37126a299c7d85febc97c539
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-29 17:05:21 -0600

    2

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,36 +1,30 @@
 #!/usr/bin/env python3
-""" Create a basic Flask App
-    with a single '/' route and an index.html template
-"""
+"""flask app"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
-from flask import g
 
 
-class Config(object):
-    """ Configure available languages in our app """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+class Config:
+    """config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-# Use Config class as config for our app
 app.config.from_object(Config)
-# Instantiate Babel object in module-level variable babel
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
-    """ Return user preferred locale, if not available return best match """
+    """get_locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
+@app.route('/')
 def index():
-    """ Return index.html template """
-    from flask_babel import gettext as _
+    """index"""
     return render_template('3-index.html')
 
 
