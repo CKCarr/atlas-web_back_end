24f942d89c2120381120d76002910c578b14c4d8
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-29 15:28:13 -0600

    test docs import in function

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -4,7 +4,6 @@
 """
 from flask import Flask, render_template, request
 from flask_babel import Babel
-from flask_babel import _  # Marking string for translation
 from flask import g
 
 
@@ -40,6 +39,7 @@ def get_locale():
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """ Return index.html template """
+    from flask_babel import _  # Marking string for translation
     return render_template('3-index.html',
                            title=_('home_title'),
                            h1=_('home_header'))
