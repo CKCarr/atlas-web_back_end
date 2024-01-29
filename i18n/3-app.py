4d9b9733896957c699fcd1419fa2cd2aff18355c
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-29 16:35:25 -0600

    test 3app

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -31,7 +31,7 @@ def get_locale():
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """ Return index.html template """
-    return render_template('3-index.html')
+    return render_template('templates/3-index.html')
 
 
 if __name__ == '__main__':
