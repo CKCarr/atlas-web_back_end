e6b8bbd1267a421e3c889875ae1d98ef6bb5f634
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-01-31 12:59:19 -0600

    test path

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -32,7 +32,7 @@ def get_locale():
 def index():
     """ Return index.html template """
     from flask_babel import gettext as _
-    return render_template('/3-index.html')
+    return render_template('./3-index.html')
 
 
 if __name__ == '__main__':
