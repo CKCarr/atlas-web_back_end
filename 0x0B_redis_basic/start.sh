1aeacbaf83fe783513a7b42f83da6813b22f4fd9
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-02-02 23:15:17 -0600

    updates to keep container running

---
+++ 0x0B_redis_basic/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Start Redis server
+service redis-server start
+
+# Check if the command argument is provided
+if [ -z "$1" ]; then
+    # If no command is provided, use `tail -f /dev/null` to keep the container running
+    tail -f /dev/null
+else
+    # If a command is provided (e.g., when using `docker exec`), execute it
+    exec "$@"
+fi
