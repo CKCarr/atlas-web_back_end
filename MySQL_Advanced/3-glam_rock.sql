8afe1de7487690957ec3bd26fd391aa4bac79e05
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-02-07 13:15:35 -0600

    had to recalculate based on the year the output in the example was given

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -10,9 +10,9 @@
 
 SELECT band_name,
     CASE 
-        WHEN split IS NULL THEN YEAR(CURDATE()) - formed 
+        WHEN split IS NULL THEN 2020 - formed 
         ELSE split - formed
     END AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC, band_name;
+ORDER BY lifespan DESC;
